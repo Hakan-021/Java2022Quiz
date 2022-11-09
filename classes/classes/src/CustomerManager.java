bf0e297606155626702e53f4f6be7806fd0aeb35
Hakan <78505691+Hakan-021@users.noreply.github.com>
2022-11-09 21:05:17 +0300

    Add files via upload

---
+++ classes/classes/src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Müşterinin eklendi");
+    }
+
+    public void Remove(){
+        System.out.println("Müşterinin silindi");
+    }
+
+    public void Update(){
+        System.out.println("Müşterinin güncellendi");
+    }
+}
